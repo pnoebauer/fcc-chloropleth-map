5c039ba515889c1084b5bfddd5277f6b20770347
Philip Noebauer <pnoebauer>
2021-08-12 18:56:02 +0800

    finished project and passed all tasks for the certificate

--- script.js
+++ script.js
@@ -46,6 +46,15 @@ svg
 		`Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)`
 	);
 
+const educationStep = 15;
+const colorMap = [
+	'rgb(16, 92, 200)',
+	'rgb(105, 180, 255)',
+	'rgb(230, 150, 84)',
+	'rgb(247, 77, 58)',
+	'rgb(255, 32, 8)',
+];
+
 const educationURL =
 	'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
 const countyURL =
@@ -66,7 +75,7 @@ async function loadAndPlotData() {
 	).features; //extract features array from object (object also contains type key, which is not required)
 
 	// console.log(countyDataTopoJson);
-	// console.log(countyData);
+	// console.log(educationData, countyData);
 
 	// attr('d') for drawing path on the canvas based on coords
 	// d3.geoPath converts coordinates into string that is passed to path
@@ -76,7 +85,86 @@ async function loadAndPlotData() {
 		.enter()
 		.append('path')
 		.attr('d', d3.geoPath())
-		.attr('class', 'county');
+		.attr('class', 'county')
+		.attr('fill', countyDataItem => {
+			const matchedEducationObject = educationData.find(
+				countyObject => countyObject.fips === countyDataItem.id
+			);
+
+			const {bachelorsOrHigher} = matchedEducationObject;
+
+			// console.log(id, matchedEducationObject);
+
+			if (bachelorsOrHigher < educationStep * 1) {
+				return colorMap[0];
+			} else if (bachelorsOrHigher < educationStep * 2) {
+				return colorMap[1];
+			} else if (bachelorsOrHigher < educationStep * 3) {
+				return colorMap[2];
+			} else if (bachelorsOrHigher < educationStep * 4) {
+				return colorMap[3];
+			}
+			return colorMap[4];
+		})
+		.attr('data-fips', countyDataItem => countyDataItem.id)
+		.attr(
+			'data-education',
+			countyDataItem =>
+				educationData.find(countyObject => countyObject.fips === countyDataItem.id)
+					.bachelorsOrHigher
+		)
+		.on('mouseover', function (mouseEvent, countyDataItem) {
+			const countyEducationData = educationData.find(
+				countyObject => countyObject.fips === countyDataItem.id
+			);
+			const {bachelorsOrHigher, area_name, state} = countyEducationData;
+
+			// d3.select(this).style('r', 8);
+			// console.log(countyDataItem, countyEducationData);
+
+			tooltip.transition().duration(200).style('opacity', 0.9);
+			tooltip
+				.html(`${area_name}, ${state}, Degree: ${bachelorsOrHigher}%`)
+				.style('text-align', 'center')
+				.attr('data-education', bachelorsOrHigher)
+				// .style('left', d.pageX + 'px')
+				.style('left', mouseEvent.offsetX + 'px')
+				.style('top', mouseEvent.offsetY + 'px');
+		})
+		.on('mouseout', function (d, i) {
+			// d3.select(this).style('fill', 'black');
+			tooltip.transition().duration(500).style('opacity', 0);
+		});
+
+	const legend = svg
+		.append('g')
+		.attr('transform', `translate(${w / 2 + padding * 2}, ${padding * 1.1})`)
+		.attr('id', 'legend');
+
+	const legendAxisScale = d3.scaleLinear().domain([0, 5]).range([0, 200]);
+
+	const lWidth = legendAxisScale(2) - legendAxisScale(1);
+
+	legend
+		.selectAll('rect')
+		.data([0, 1, 2, 3, 4])
+		.enter()
+		.append('rect')
+		.attr('height', 10)
+		.attr('width', lWidth)
+		.attr('x', (d, i) => legendAxisScale(i))
+		.attr('fill', (d, i) => colorMap[i]);
+
+	const legendXAxis = d3
+		.axisBottom(legendAxisScale)
+		.tickFormat((x, i) => (i === 0 || i === 5 ? '' : `${i * educationStep}%`))
+		.ticks(5);
+
+	legend
+		.append('g')
+		.attr('transform', `translate(0, 10)`)
+		.attr('id', 'legend-x-axis')
+		.call(legendXAxis);
 }
 
 loadAndPlotData();
